075f9f6496cbd5631d70f30f952bf7e298d2e4ef
Kavin P <kpcloud4@gmail.com>
2025-11-17 21:51:37 +0530

    Create the setup

---
+++ setup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python -c "import nltk; nltk.download('punkt'); nltk.download('stopwords'); nltk.download('averaged_perceptron_tagger'); nltk.download('maxent_ne_chunker'); nltk.download('words')"
